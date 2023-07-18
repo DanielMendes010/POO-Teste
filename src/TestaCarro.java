de57d34f5362850ff43dd28527e6c0fbc18082bf
DanielMendes010 <danielinacio101@gmail.com>
2023-07-18 10:12:43 -0300

    Adicionando outro carro

--- src/TestaCarro.java
+++ src/TestaCarro.java
@@ -1,7 +1,9 @@
 public class TestaCarro {
     public static void main(String[] args) {
         Carro carro1 = new Carro("chevrolet", "omega", 2015, "preto", 1.0);
+        Carro carro2 = new CarroAutomatico("citroen", "xcsara picasso", 2005, "preto", 2.0);
 
-        System.out.println(carro1);
+        System.out.println("Carro 1: " + carro1);
+        System.out.println("Carro 2: " + carro2);
     }
 }
